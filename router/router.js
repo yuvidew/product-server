725164851e5053d2fc92ec6e6533edd5a670223a
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-03-11 23:29:25 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { getAllProducts } = require('../controller/products')
+const { getAllProducts  , postProduct} = require('../controller/products')
 const router = express.Router()
 
 router.get('/v1/products' , getAllProducts)
+router.post('/v1/add/product' , postProduct)
 
 module.exports = router
