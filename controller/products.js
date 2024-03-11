ca0f7e90bb77190cb23acc432509bf294b5271df
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-03-11 23:30:11 +0530

    Update products.js

--- controller/products.js
+++ controller/products.js
@@ -5,8 +5,19 @@ const getAllProducts = async (req , res) => {
     return res.status(200).json(data)
 }
 
+const postProduct = async (req , res) => {
+    const {body} = req
+    try {
+        const result = await proDataSchema.create(body)
+        if(result) return res.status(200).json(result)
+    } catch (error) {
+        return res.status(404).json(error)
+    }
+}
+
 
 module.exports = {
     getAllProducts , 
+    postProduct
 }
 
